6fc6765ce792d36952b06a4579e4ce8c7806b999
Anita <smirnovasiberia@gmail.com>
2019-04-02 18:01:55 +0300

    "templates added"

--- django/questions/views.py
+++ django/questions/views.py
@@ -1,22 +1,67 @@
+import random
+
 from django.shortcuts import render
 
+
 def home_page(request):
-    return render(request, 'questions/index.html', {})
+    data = generate_question_list(30)
+    question_list = {"question_list": data}
+    return render(request, 'questions/index_authorized.html', context=question_list)
+
 
 def ask(request):
     return render(request, 'questions/ask.html', {})
 
+
 def signup(request):
     return render(request, 'questions/signup.html', {})
+
 
 def login(request):
     return render(request, 'questions/login.html', {})
 
+
 def one_question(request):
-    return render(request, 'questions/question.html', {})
+    data = generate_answers_list()
+    question = generate_question_list(1)
+    data = {"question": question, "answers": data}
+    return render(request, 'questions/question.html', context=data)
+
 
 def tag(request):
     return render(request, 'questions/tag.html', {})
 
+
 def hot(request):
-    return render(request, 'questions/hot_list.html', {})
+    data = generate_question_list(30)
+    question_list = {"question_list": data}
+    return render(request, 'questions/hot_list.html', context=question_list)
+
+
+def generate_question_list(questions_amount):
+    questions = []
+    for i in range(1, questions_amount):
+        tags = ['tag' + str(random.randint(1, 20)), 'tag' + str(random.randint(1, 20))]
+
+        questions.append({
+            'title': 'title' + str(i),
+            'id': i,
+            'tags': tags,
+            'text': 'text' + str(i),
+            'rating': random.randint(1, 50),
+            'answers_amount': random.randint(1, 15)
+
+        })
+    return questions
+
+
+def generate_answers_list():
+    questions = []
+    for i in range(1, 7):
+        questions.append({
+            'title': 'answer' + str(i),
+            'id': i,
+            'text': 'text answer' + str(i),
+            'rating': random.randint(1, 50),
+        })
+    return questions
