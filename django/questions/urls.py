e217e626d17b689390a2212ff61becf81bc05c31
Anita <smirnovasiberia@gmail.com>
2019-05-27 22:02:57 +0300

    urls changed

--- django/questions/urls.py
+++ django/questions/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
+
 from . import views
-from django.conf.urls import url
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('ask/', views.ask, name='ask'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-    #path
-    url(r'^question/(?P<q_id>[0-9]+)$', views.one_question, name='one_question'),
-    path('tag/(?P<tag_name>[\w\-]+)', views.tag, name='tag'),
+    path(r'question/<int:q_id>', views.one_question, name='one_question'),
+    path('tag/<slug:tag_name>', views.tag, name='tag'),
     path('hot/', views.hot, name='hot'),
     path('settings/', views.settings, name='settings'),
 ]
